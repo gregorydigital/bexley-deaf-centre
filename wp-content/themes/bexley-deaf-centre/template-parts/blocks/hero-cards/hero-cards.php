dc1f7bf4a1d2a60bc274e366f4249cdd517b097e
Michael Gregory <michael.gregory@transmissonagency.com>
2025-10-16 09:38:47 +0100

    yeh yeh

--- wp-content/themes/bexley-deaf-centre/template-parts/blocks/hero-cards/hero-cards.php
+++ wp-content/themes/bexley-deaf-centre/template-parts/blocks/hero-cards/hero-cards.php
@@ -1,16 +1,10 @@
-<?php 
-    $title = get_field('title');
-    $text = get_field('text');
-    $bg_color = get_field('background_color');
-?>
-
 <?php if ( ! empty( $block['data']['_is_preview'] ) ) : ?>
 
-    <img style='max-width:100%;height:auto;' src='<?php echo esc_url( get_template_directory_uri() . '/template-parts/blocks/ /_preview.png' ); ?>'>
+    <img style='max-width:100%;height:auto;' src='<?php echo esc_url( get_template_directory_uri() . '/template-parts/blocks/hero-cards/_preview.png' ); ?>'>
 
 <?php else: ?>
 
-    <section class='hero-cards padded-mid bg-<?php echo esc_html($bg_color)?>'>
+    <section class='hero-cards padded-mid'>
         <div class='container'>
             <div class='hero-cards__inner' data-aos="fade-up">
                 <?php if(have_rows('hero_cards')): ?>
@@ -19,21 +13,27 @@
                             $title = get_sub_field('title');
                             $text = get_sub_field('text');
                             $link = get_sub_field('link');
+                            $image = get_sub_field('image');
                         ?>
-                        <div class="hero-cards__card">
-                            <?php if(!empty($title)): ?>
-                                <h3><?php echo esc_html($title); ?></h3>
+                        <div class="hero-cards__card <?php echo $image ? 'bg-image' : '';?>">
+                            <?php if (!empty($image)) : ?>
+                                <img class="img-object-fit" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt'] ?? ''); ?>" />
                             <?php endif; ?>
-                            <?php if(!empty($text)): ?>
-                                <p><?php echo esc_html($text); ?></p>
-                            <?php endif; ?>
-                            <?php if( $link ):
-                                $link_url = $link['url'];
-                                $link_title = $link['title'];
-                                $link_target = $link['target'] ? $link['target'] : '_self';
-                            ?>
-                                <a class='btn btn--white' href='<?php echo esc_url( $link_url ); ?>' target='<?php echo esc_attr( $link_target ); ?>'><?php echo esc_html( $link_title ); ?></a>
-                            <?php endif; ?>
+                            <div class="hero-cards__content">
+                                <?php if(!empty($title)): ?>
+                                    <h3><?php echo esc_html($title); ?></h3>
+                                <?php endif; ?>
+                                <?php if(!empty($text)): ?>
+                                    <p><?php echo esc_html($text); ?></p>
+                                <?php endif; ?>
+                                <?php if( $link ):
+                                    $link_url = $link['url'];
+                                    $link_title = $link['title'];
+                                    $link_target = $link['target'] ? $link['target'] : '_self';
+                                ?>
+                                    <a class='btn btn--white' href='<?php echo esc_url( $link_url ); ?>' target='<?php echo esc_attr( $link_target ); ?>'><?php echo esc_html( $link_title ); ?></a>
+                                <?php endif; ?>
+                            </div>
                         </div>
                     <?php endwhile; ?>
                 <?php endif; ?>
